c2448d253dd21355356e8c3de8165858eb26fb95
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-03-25 19:16:15 +0900

    Create training.php

---
+++ training.php
@@ -0,0 +1,32 @@
+<?php
+    
+    include_once 'navigation.php'
+
+?>
+
+
+<div class="training" id="training">
+           
+            <div id="table-wrapper3">
+                <div id="table-scroll3">
+                    <table id="trainingTable" border="1" class="trainingT">
+                    <thead>
+                    <tr id="firstrow">
+                    <th style="width:15%">No</th>
+                    <th style="width:40%">ファイル名</th>
+                    <th style="width:20%">Description</th>
+                    <th style="width:25%">Completion</th>
+
+                    </thead>
+                    <tr><td>Training 004</td><td>SG031_20240206_教育訓練記録についての教育(その他)</td><td>Description Sample</td><td>完了</td></tr>
+                    <form action="includes/complete.inc.php" method="post">
+                    <tr><td>Training 005</td><td>SG032_20240209_教育訓練記録についての教育(安全衛生)</td><td>Description 2</td><td><button>Complete</button></td></tr></form>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
