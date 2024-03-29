8cf4f325f8844707d45f893b901ff4ed6f21cbb3
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-03-29 17:02:48 +0900

    Update includes_attendanceTap.php

--- includes_attendanceTap.php
+++ includes_attendanceTap.php
@@ -12,46 +12,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         require_once "dbh2.inc.php";
 
-       /* $query = "UPDATE attendance, signdb
-        SET attendance.GID = signdb.GID
-
-        where attendance.RFID = :rfid;"; */
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, 1);
 
-        $query ="UPDATE sg032
+        $query ="UPDATE attendance
                         
-        INNER JOIN signdb ON sg032.RFID = signdb.RFID
+        INNER JOIN signdb ON attendance.RFID = signdb.RFID
         SET
-            sg032.GID = signdb.GID
+            attendance.GID = signdb.GID
             
         WHERE
-            sg032.RFID = :rfid
-            AND sg032.training_id = :documentNo;
+            attendance.RFID = :rfid
+            AND attendance.training_id = :documentNo;
             
-
-        UPDATE sg032
+        UPDATE attendance
             SET 
-            dateID = Now()
+            date_id = Now()
 
         WHERE 
             RFID = :rfid
-            AND sg032.training_id = :documentNo;";
-            
-
-       /* $query = "UPDATE currentstatus
-        SET KP = :maintenanceid,
-        statusid = :statusid
-        where machineID = :machineid;";*/
-      
+            AND attendance.training_id = :documentNo;";
+                
         $stmt = $pdo->prepare($query);
 
         $stmt->bindParam("rfid", $rfid);
         $stmt->bindParam("documentNo", $documentNo);
-        
-        
-        
-      
-                     
+                          
         $stmt->execute();
 
         $pdo = null;
@@ -65,7 +50,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Query failed: " . $e->getMessage());
         
     }
-
   
 }
 else {
