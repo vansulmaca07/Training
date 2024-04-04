571dd5e88e353566b8a37bd7222284d8bd1881ca
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-04-04 16:56:25 +0900

    Update idregister.php

--- idregister.php
+++ idregister.php
@@ -1,76 +1,51 @@
 <?php
 
-session_start()
 
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-        
-<div class="idregistration" id="idregistration">
-            <h4>IDカード登録</h4>
-            <h5>Please confirm the following</h5>
-            <div id="table-wrapper5">
-                <div id="table-scroll5">
-                    <table id="IDregTable" border="1" class="IDregT">
-                        <thead>
-                    <tr id="firstrow">
-                    <th style="width:25%">GID</th>
-                    <th style="width:25%">名前</th>
-                    <th style="width:25%">工程</th> <!--process--> 
-                    </thead>
-                    
-                    <?php
-                    $servername = "localhost";
-                        $username = "root";
-                        $password = "";
-                        $database = "trainingdb";
-                        
-                        //Create connection
-
-                        $connection = new mysqli($servername, $username, $password, $database);
-
-                        //Check connection
-
-                        if ($connection->connect_error) {
-                            die("Connection failed: " . $connection->connect_error);
-                        }
-                        
-                        //read all row from database table
-
-                        //$sql = "SELECT * FROM '{$_SESSION["documentNo"]}'";
-                        $sql = "SELECT * FROM users where department = '1' AND GID = 'jt9970440'";
-                        
-                        $result = $connection->query($sql);
-                        
-                        if (!$result) {
-                            die("Invalid Query: " . $connection->error);
-                        }
-
-                        //read data of each row
-                        while ($row = $result->fetch_assoc()){
-
-                        echo "<tr>
-                            <td>" . $row["GID"] .  "</td>
-                            <td>" . $row["firstname"] .  "</td>
-                            <td>" . $row["department"] .  "</td>";
-                                        
-                        }
-                        
-                        ?>
-                        <form action="includes/idregister.php" method="post"> 
-                        <label for="IDregistrationInput"></label>
-                        <input type="text" id="IDregistrationInput" class="IDregistrationInput" name="IDregistrationInput" value="" placeholder="Please Tap your ID">
-                        <input type="submit" class="btn2" id="submit" name="submit">
-                        </form>
-                        
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
-</body>
-</html>
+
+    $rfid = ($_POST["rfid"]);
+    $GIDfetch  = ($_POST["GIDfetch"]);
+   
+    
+    try {
+        require_once "dbh2.inc.php";
+
+       
+       $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, 1);
+
+        $query ="UPDATE users
+                        
+        SET
+            RFID = :rfid    
+        WHERE
+            GID = :GIDfetch;";
+    
+        $stmt = $pdo->prepare($query);
+        
+        $stmt->bindParam(":rfid", $rfid);
+        $stmt->bindParam(":GIDfetch", $GIDfetch);
+          
+        $stmt->execute();
+
+        $pdo = null;
+        $stmt = null;
+
+       
+        header('location: ../idregistration.php');
+
+        die();
+        
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+        
+    }
+
+}
+else {
+    header("Location: ../idregistration.php");
+}
+
+
