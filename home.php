1538ef7a2a3d45422e9c8bbf007e5773dcb6fa29
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-04-04 16:58:44 +0900

    Update home.php

--- home.php
+++ home.php
@@ -208,30 +208,7 @@
             </div>
         </div>     -->
      
-        <div class="progress" id="progress" style="display: none">
-            <div id="table-wrapper">
-                <div id="table-scroll">
-                    <table id="progressTable" border="1" class="progressT">
-                        <thead>
-                    <tr id="firstrow">
-                    <th style="width:15%">No</th>
-                    <th style="width:40%">ファイル名</th>
-                    <th style="width:15%">全体状態</th>
-                    <th style="width:15%">備考欄</th>
-                    <th style="width:15%">【サイン進捗】</th>
-                    
-                    </thead>
-                    <tr><td>SG026</td><td>SG026_20231221_上級監督者教育(安全衛生その他)</td><td style="bgcolor:green">完了</td><td></td><td>完了</td></tr>
-                    <tr><td>SG027</td><td>SG027_20240105_通勤災害の共有、通災MAPの教育</td><td>完了</td><td></td><td>完了</td></tr>
-                    <tr><td>SG028</td><td>SG028_20240110_熱戻し炉常時監視、省エネモード　取り扱い説明(安全衛生)</td><td>完了</td><td></td><td>完了</td></tr>
-                    <tr><td>SG029</td><td>SG029_20240110_通勤災害についての教育(安全衛生)</td><td>完了</td><td></td><td>完了</td></tr>
-                    <tr><td>SG030</td><td>SG030_20240111_AB工程　異物検出・テープ貼り位置検出カメラ教育(その他)</td><td>完了</td><td></td><td>完了</td></tr>
-                    <tr><td>SG031</td><td>SG031_20240206_教育訓練記録についての教育(その他)</td><td>完了</td><td></td><td>完了</td></tr>
-                    <tr><td>SG032</td><td>SG032_20240209_教育訓練記録についての教育(安全衛生)</td><td>進行中</td><td></td>
-                    <td><a href="Attendance.php"><button>Attendance</button></a> </td></tr>
-                    </table>
-                 
-                </div>
+       
             </div>
         </div>
         <div class="training" id="training" style="display: none">
