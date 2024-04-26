2aefcd9c72d8885a36f776e32328382c2cc0e306
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-04-26 16:18:14 +0900

    Update progress.php

--- progress.php
+++ progress.php
@@ -1,96 +1,96 @@
 <?php  
-    include_once 'navigation.php'
+    include_once 'navigation.php';
+    include("includes/dbh2.inc.php");
 ?>
 
-<div class="progress" id="progress">
+        <div class="progress" id="progress">
             <h4>進捗状況</h4>
-            <div id="table-wrapper">
-                <div id="table-scroll">
-                    <table id="progressTable" border="1" class="progressT">
-                        <thead>
-                    <tr id="firstrow">
-                    <th style="width:10%">No</th>
-                    <th style="width:10%">作成者</th> <!--creator-->
-                    <th style="width:20%">ファイル名</th>
-                    <th style="width:10%">区分</th>
-                    <th style="width:20%">教材</th>
-                    <th style="width:10%">全体状態</th>
-                    <th style="width:10%">備考欄</th>
-                    <th style="width:10%">【サイン進捗】</th>
-                    </thead>
-                    
-                    <?php
-                    $servername = "localhost";
-                        $username = "root";
-                        $password = "";
-                        $database = "trainingdb";
-                        
-                        //Create connection
-
-                        $connection = new mysqli($servername, $username, $password, $database);
-
-                        //Check connection
-
-                        if ($connection->connect_error) {
-                            die("Connection failed: " . $connection->connect_error);
-                        }
-                        
-                        //read all row from database table
-
-                        //$sql = "SELECT * FROM '{$_SESSION["documentNo"]}'";
-                        $department = $_SESSION["department"];
-                        $group_ = $_SESSION["group_"];
-                        $sql = "SELECT document_id, training_name, status_name, creator, name_, category, usage_id
-                        from training_form 
-
-                        inner join status_ref on training_form.status_id = status_ref.status_id
-                        inner join users on training_form.creator = users.GID
-
-                        where training_form.creation_department ='$department';"; 
-
-                        $result = $connection->query($sql);
-
-                        if (!$result) {
-                            die("Invalid Query: " . $connection->error);
-                        }
-
-                        //read data of each row
-                        while ($row = $result->fetch_assoc()){
-
-                        echo "<tr>
-                            <td>" . $row["document_id"] .  "</td>
-                            <td>" . $row["creator"] .  "</td>
-                            <td>" . $row["training_name"] .  "</td>
-                            <td>" . $row["category"] .  "</td>
-                            <td>" . $row["usage_id"] .  "</td>
-                            <td>" . $row["status_name"] .  "</td>
-                            <td></td>
-                            <td><form action='includes/documentNo.inc.php' method ='POST'>
-                            <input type='text' hidden value='$row[document_id]' name='documentNo'>             
-                            <button type='submit'>サイン</button>
-                            </form>
-                            </td>
-                            
-                            </tr>";
-                        }
-                        
-                        ?>          
-             
-                    </table>        
+                <div id="table-wrapper">
+                    <div id="table-scroll"> 
+                        <table id="progressTable" border="1" class="table table-hover rounded-3 overflow-hidden progressT">
+                            <thead>
+                                <tr id="firstrow">
+                                    <th style="width:10%; vertical-align:middle;">No</th>
+                                    <th style="width:15%; vertical-align:middle;">作成者</th> <!--creator-->
+                                    <th style="width:20%; vertical-align:middle;">ファイル名</th>
+                                    <th style="width:15%; vertical-align:middle;">
+                                        <a href="" role="button" id="drowdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false" style="color:white" class="dropdown-toggle">区分</a>
+                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                                <?php
+                                                    $query = "SELECT DISTINCT(category) FROM training_form
+                                                        ;";
+                                                    $stmt = $pdo->prepare($query);
+                                                    $stmt->execute();
+                                                    $result = $stmt->fetchAll();
+                                                    foreach($result as $row)
+                                                        {
+                                                ?>
+                                                    <div class="list-group-item checkbox">
+                                                        <label><input type="checkbox" class="common_selector category" value="
+                                                            <?php echo $row["category"];
+                                                            ?>
+                                                            "> <?php echo $row["category"];
+                                                                ?>
+                                                        </label>
+                                                    </div>
+                                                        <?php
+                                                        }
+                                                        ?> 
+                                            </ul>   
+                                    </th>
+                                    <th style="width:20%; vertical-align:middle;">使用資料</th>
+                                    <th style="width:10%; vertical-align:middle;">全体状態</th>
+                                    <th style="width:10%; vertical-align:middle;">【サイン進捗 </th>
+                                </tr>
+                            </thead>
+                            <tbody id="post_list2">
+                            </tbody>
+                        </table>        
+                    </div>
                 </div>
-            </div>
         </div>
-        <div class="training" id="training" style="display: none">
-        <p>TRAINING</p>
-        </div>
-        <div class="regsignature" id="regsignature" style="display: none">
-        <p>REGISTER SIGNATURE</p>
-        </div>
-        <div class="usermanagement" id="usermanagement" style="display: none">
-        <p>USER MANAGEMENT</p>
-            </div>
-            </div>
-    </div>
+    </div> <!--mainwrapper-->
+</div> <!--full-->
+
+<script type="text/javascript">
+
+$(document).ready(function() {
+
+  filter_data();
+
+  function filter_data() {
+     // $('.filter_data').html();
+      $('#post_list2').html();
+      var action = 'fetch_data';
+      var category = get_filter('category');
+    
+      $.ajax({
+          url: "includes/fetch_data_progress.inc.php",
+          method: "POST",
+          data: {action:action, category:category},
+          success:function(data){
+           // $('.filter_data').html(data);
+            $('#post_list2').html(data);
+          }
+      });
+  }
+
+  function get_filter(class_name)
+  {
+    var filter = [];
+    $('.'+class_name+':checked').each(function(){
+        filter.push($(this).val());
+    });
+
+    return filter;
+  }
+
+  $('.common_selector').click(function(){
+      filter_data();
+  });
+});
+
+</script>
 </body>
 </html>
 
