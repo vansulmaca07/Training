b6287e9dbd4d407010d1db9907dfa768367a9a61
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-04-26 16:17:33 +0900

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include("includes/dbh2.inc.php");
 ?>
 
 <!DOCTYPE html>
@@ -8,70 +9,85 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!--jQuery and Bootstrap CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="home.css">
+
+    <!--jQuery and Bootstrap javascript-->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+    <!--Icon Scout-->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+
     <title>Home</title>
 </head>
 <body>
     <div class="full">
         <div class="menuDIV">
             <div class="loginDIV">
-                    <p class="loginP">
-                        <?php
+                <p class="loginP" style="margin-top:15px; margin-bottom:0px;">
+                    <?php
                         echo
                         $_SESSION["name_"];
                     ?>
-                    </p>
-                    <p class="loginP">    <?php
+                </p>
+                <p class="loginP" style="margin:0px;">
+                    <?php
                         echo $_SESSION["GID"];
-                    ?></p>
-                    <p class="loginP">    <?php
-
+                    ?>
+                </p>
+                <p class="loginP" style="margin:0px;">    
+                    <?php
                         if ($_SESSION["userlevel"] === "1"){
                         echo "administrator";
                         }
-                        else {
-                            
+                        else {        
                         }
-                    ?></p>
-                     <p class="loginP">    <?php
+                    ?>
+                </p>
+                <p class="loginP" style="margin:0px;">    
+                    <?php
                         echo $_SESSION["department_name"];
-                    ?></p>
-                    
-                    <a href="includes/logout.inc.php"> <button class="btn">ログアウト</button> </a>        
+                    ?>
+                </p>  
+                    <a href="includes/logout.inc.php" class="btn-34">ログアウト</a>   
             </div>
             <div class="navDIV">
-
                 <?php
-                
                 if($_SESSION["userlevel"] === "1") {
 
                 echo
                  '
-                 <a href="progress.php" class = "btn2">進捗状況</a>
-                 <a href="newform.php" class = "btn2">新規作成</a>
-                 <a href="training.php" class = "btn2">訓練</a>
-                 <a href="approval.php" class = "btn2">承認</a>
-                 <a href="idregistration.php" class = "btn2">IDカード登録</a>
-                 <a href="" class = "btn2">ユーザー管理</a>';
+                 <a href="progress.php" class = "btn-34">進捗状況</a>
+                 <a href="newform.php" class = "btn-34">新規作成</a>
+                 <a href="training.php" class = "btn-34">訓練</a>
+                 <a href="approval.php" class = "btn-34">確認者確認・提出</a>
+                 <a href="idregistration.php" class = "btn-34">IDカード登録</a>
+                 <a href="" class = "btn-34">ユーザー管理</a>'
+                 ;
                 }
 
                 else if ($_SESSION["userlevel"] === "3") {
                 echo 
                 '<button class="btn4" onclick="displayRegForm()" disabled>新規作成</button></a>
                 <button class="btn4" onclick="displayProgress()" disabled>進捗状況</button></a>
-                <a href="training.php" class = "btn2">訓練</a>
-                <a href="approval.php" class = "btn4" disabled>承認</a>
-                <button class="btn4" onclick="regSignature()" disabled >Namelist Master</button></a>
+                <a href="training.php" class = "btn-34">訓練</a>
+                <button class="btn4" onclick="regSignature()" disabled >確認者確認・提出</button></a>
+                <button class="btn4" onclick="regSignature()" disabled >IDカード登録</button></a>
                 <button class="btn4" onclick="userManagement()" disabled>ユーザー管理</button></a>';
                 }
                 
                 else if ($_SESSION["userlevel"] === "2") {
                 echo 
-                '<a href="progress.php" class = "btn2">進捗状況</a>
-                 <a href="newform.php" class = "btn2">新規作成</a>
-                 <a href="training.php" class = "btn2">訓練</a>
-                 <a href="approval.php" class = "btn2">承認</a>
-                 <a href="idregistration.php" class = "btn2">IDカード登録</a>
+                '<a href="progress.php" class = "btn-34">進捗状況</a>
+                 <a href="newform.php" class = "btn-34">新規作成</a>
+                 <a href="training.php" class = "btn-34">訓練</a>
+                 <a href="approval.php" class = "btn-34">確認者確認・提出</a>
+                 <a href="idregistration.php" class = "btn-34">IDカード登録</a>
                  <a href="" class = "btn4" disabled>ユーザー管理</a>';
                 }
                 ?>
