932ae3fc7018fa2339c8f3eb54cdff13cd834bae
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-05-01 17:05:30 +0900

    Update fetch_data_progress.inc.php

--- fetch_data_progress.inc.php
+++ fetch_data_progress.inc.php
@@ -13,7 +13,32 @@ if(isset($_POST["action"])) {
    // INNER JOIN shift on users.shift_id = shift.shift_id
    // WHERE users.group_ ='$group_'";
 
-    $query = "SELECT document_id, training_name, status_name, creator, name_, category, usage_id
+    $query_count_complete = "SELECT sign_progress, count(*) from attendance WHERE training_id = 'SG001' and sign_progress = '2' GROUP BY sign_progress ORDER BY COUNT(*) DESC;
+    ";
+    $stmt_02 = $pdo->prepare($query_count_complete);
+    $stmt_02->execute();
+    $result_02 = $stmt_02->fetchAll();
+
+    $complete_count='';
+
+    foreach($result_02 as $row_02)
+    {
+        $row02["count(*)"]=$complete_count;
+    }
+
+    $query_count_total = "SELECT sign_progress, count(*) from attendance WHERE training_id = 'SG001' ;";
+    $stmt_03 = $pdo->prepare($query_count_total);
+    $stmt_03->execute();
+    $result_03 = $stmt_03->fetchAll();
+
+    $total_count='';
+
+    foreach($result_03 as $row_03)
+    {
+        $row_03["count(*)"]=$total_count;
+    }
+
+    $query = "SELECT training_id, training_name, status_name, creator, name_, category, usage_id
     from training_form 
 
     inner join status_ref on training_form.status_id = status_ref.status_id
@@ -39,15 +64,21 @@ if(isset($_POST["action"])) {
             $output .= 
                 "<tr>
                             <td style='vertical-align: middle;'><form action = 'includes/edit_form.inc.php' method ='post' id='edit_form'>
-                            <input type='text' hidden name= 'training_id' value = '$row[document_id]'><button type='submit' class='btn-link'>" . $row["document_id"] .  "</button>
+                            <input type='text' hidden name= 'training_id' value = '$row[training_id]'><button type='submit' class='btn-link'>" . $row["training_id"] .  "</button>
                             </form></td>
                             <td style='vertical-align: middle;'>" . $row["name_"] .  "</td>
                             <td style='vertical-align: middle;'>" . $row["training_name"] .  "</td>
                             <td style='vertical-align: middle;'>" . $row["category"] .  "</td>
                             <td style='vertical-align: middle;'>" . $row["usage_id"] .  "</td>
                             <td style='vertical-align: middle;'>" . $row["status_name"] .  "</td>
+                            <td style='vertical-align: middle;'>
+                                <div class='progress'>
+                                <div class='progress-bar progress-bar-striped progress-bar-animated bg-success' role='progressbar' style='width: 50%' aria-valuenow='25' aria-valuemin='0' aria-valuemax='100'></div>
+                                </div>
+                                </td>
                             <td style='vertical-align: middle;'><form action='includes/documentNo.inc.php' method ='POST'>
-                            <input type='text' hidden value='$row[document_id]' name='documentNo'>             
+
+                            <input type='text' hidden value='$row[training_id]' name='documentNo'>             
                             <button type='submit' class='btn'>サイン</button>
                             </form>
                             </td>
@@ -60,6 +91,5 @@ if(isset($_POST["action"])) {
     }
 
     echo $output;
-   
    
 }
