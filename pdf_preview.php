b20e068ddb69296d2051001d1b30dd7172e4247c
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-05-08 17:05:42 +0900

    Create pdf_preview.php

---
+++ pdf_preview.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="pdf_preview.css" class="href">
+
+</head>
+<body>
+    <div class="book" id="test">
+        <div class="page" id="page">
+            <div class="subpage">
+                <div class="header-container">
+                    <div class="header-1" style="width:100%;">
+                        <div class="main_title">
+                            <h3><b>教育・訓練記録（製造）</b></h3>
+                        </div>
+                        <div class="creation_department" id="creation_department">
+                            <h3><b>作成部署:</b></h3>
+                        </div>
+                    </div>
+                    <div class="header-2" style="width:27%; text-align:center;">
+                        <span style=""><u>NO.___________</u></span>
+                        <table style="width:100%">
+                            <tr>
+                                <td style="width:50%; text-align:center;">
+                                    <span>確　認</span>
+                                </td>
+                                <td style="width:50%; text-align:center;">
+                                    <span>作　成</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:75px;"></td>
+                                <td style="height:75px;"></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="main_record" style="margin-top:20px;">
+                    <table id="main_record" class="main_record" style="width:100%; margin-top: 10px;">
+                        <tr>
+                            <td style="width:70%;">
+                                <span>名称：</span>
+                            </td>
+                            <td style="width:15%; text-align:center;"> 
+                                <span>社内</span>
+                            </td>
+                            <td style="width:15%; text-align:center;">
+                                <span>社外</span>
+                            </td>
+                        </tr>
+                    </table>
+                    <table id="main_information" class="main_information" style="width:100%; border-top:0;">
+                        <tr>
+                            <td style="width:17%;">
+                                <span>日勤者実施日時：</span>
+                            </td>
+                            <td style="width:20%; text-align:center;"> 
+                                <span></span>
+                            </td>
+                            <td style="width:20%; text-align:center;">
+                                <span></span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>場所：</span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>講師：</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width:17%;">
+                                <span>Ａ班実施日時： </span>
+                            </td>
+                            <td style="width:20%; text-align:center;"> 
+                                <span></span>
+                            </td>
+                            <td style="width:20%; text-align:center;">
+                                <span></span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>場所：</span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>講師：</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width:17%;">
+                                <span>Ｂ班実施日時：   </span>
+                            </td>
+                            <td style="width:20%; text-align:center;"> 
+                                <span></span>
+                            </td>
+                            <td style="width:20%; text-align:center;">
+                                <span></span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>場所：</span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>講師：</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width:17%;">
+                                <span>Ｃ班実施日時：</span>
+                            </td>
+                            <td style="width:20%; text-align:center;"> 
+                                <span></span>
+                            </td>
+                            <td style="width:20%; text-align:center;">
+                                <span></span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>場所：</span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>講師：</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width:17%;">
+                                <span>Ｄ班実施日時：</span>
+                            </td>
+                            <td style="width:20%; text-align:center;"> 
+                                <span></span>
+                            </td>
+                            <td style="width:20%; text-align:center;">
+                                <span></span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>場所：</span>
+                            </td>
+                            <td style="width:20%;">
+                                <span>講師：</span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="category" style="margin-top:10px;">
+                    <caption>区分（該当項目を丸枠で囲んで下さい。）</caption>
+                    <table id="category_table" class="category_table" style="width:100%">
+                        <tr>
+                            <td style="width:25%;">
+                                <span>品質</span>
+                            </td>
+                            <td style="width:25%; text-align:center;">
+                                <span>環境</span>                                   
+                            </td>
+                            <td style="width:25%; text-align:center;">
+                                <span>安全衛生</span>                               
+                            </td>
+                            <td style="width:25%; text-align:center;">
+                                <span>その他:</span>                               
+                            </td>
+                        </tr>
+                    </table>
+                    ※安全衛生に関係する教育の場合、原本を総務部門に提出し、作成部署はコピーを保管する。
+                </div>
+                <div class="purpose"  style="margin-top:10px;">
+                    <caption>目的、対象者</caption>
+                    <table id="purpose_table" class="purpose_table" style="width:100%">
+                        <tr>
+                            <td colspan="2" style="width:100%;">
+                                <span>目的：</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width:25%;">
+                                <span>対象者：</span>
+                            </td>
+                            <td style="width:25%;">
+                                <span>名：</span>                                   
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="contents"  style="margin-top:10px;">
+                    <caption>内容</caption>
+                    <table id="contents_table" class="contents_table" style="width:100%">
+                        <tr>
+                            <td style="width:100%; height: 70px;">
+                                <span></span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="usage_materials"  style="margin-top:5px;">
+                    <table id="usage_material_table" class="usage_material_table" style="width:100%">
+                        <tr>
+                            <td style="width:100%;">
+                                <span>使用資料　（作業標準がある場合には、作業標準№を記入、ない場合には資料名等を記入）</span>
+                            </td> 
+                        </tr>
+                        <tr>
+                            <td style="width:100%; height: 40px;">
+                                <span></span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="methods_schedule"  style="margin-top:10px;">
+                    <caption>教育効果の確認方法、確認予定日</caption>
+                    <table id="methods_schedule_table" class="methods_schedule_table" style="width:100%">
+                        <tr>
+                            <td style="width:100%;">
+                                <span>インタビュー式による確認</span>
+                            </td> 
+                        </tr>
+                        <tr>
+                            <td style="width:100%;">
+                                <span>最終確認予定日：</span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="confirmation" style="width:100%; margin-top:10px;">
+                    <div class="column-1" style="width:80%">
+                        <caption>教育効果の確認結果</caption>
+                        <table class="result_confirmation_regular" id="result_confirmation_regular" style="width:100%; margin-top:30px;">
+                            <tr>
+                               <td><span>日勤者：</span></td>
+                            </tr>
+                            <tr>
+                               <td><span>上記の人にインタビューを実施し、理解できたことを確認できました。</span></td>
+                            </tr>
+                        </table>
+                        <table class="result_confirmation_regular" id="result_confirmation_regular" style="width:100%;  margin-top:30px;">
+                            <tr>
+                               <td><span>Ａ班：</span></td>
+                            </tr>
+                            <tr>
+                               <td><span>上記の人にインタビューを実施し、理解できたことを確認できました。</span></td>
+                            </tr>
+                        </table> 
+                        <table class="result_confirmation_regular" id="result_confirmation_regular" style="width:100%;  margin-top:30px;">
+                            <tr>
+                               <td><span>Ｂ班：</span></td>
+                            </tr>
+                            <tr>
+                               <td><span>上記の人にインタビューを実施し、理解できたことを確認できました。</span></td>
+                            </tr>
+                        </table> 
+                        <table class="result_confirmation_regular" id="result_confirmation_regular" style="width:100%; margin-top:30px;">
+                            <tr>
+                               <td><span>Ｃ班：</span></td>
+                            </tr>
+                            <tr>
+                               <td><span>上記の人にインタビューを実施し、理解できたことを確認できました。</span></td>
+                            </tr>
+                        </table>
+                        <table class="result_confirmation_regular" id="result_confirmation_regular" style="width:100%; margin-top:30px;">
+                            <tr>
+                               <td><span>Ｄ班：</span></td>
+                            </tr>
+                            <tr>
+                               <td><span>上記の人にインタビューを実施し、理解できたことを確認できました。</span></td>
+                            </tr>
+                        </table>     
+                    </div>
+                    <div class="column-2" style="width:20%">
+                        <table class="effect_checker" style="float:right; width: 75%; text-align:center;">
+                            <tr>
+                                <td><span>効果確認者</span></td>
+                            </tr>
+                            <tr>
+                                <td style="height: 75px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="height: 75px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="height: 75px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="height: 75px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="height: 75px;"></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div> 
+                
+            </div>
+        </div>
+        <div class="page">
+            <div class="subpage">Page 2/2</div>
+        </div>
+    </div>
+    
+
+    <button id="btn-print-this" class="btn btn-primary">PDF SAVE</button>
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
+    
+</body>
+
+<script src="printThis.js"></script>
+<script>
+
+$(document).ready(function(){
+    $('#btn-print-this').click(function(){
+        $('#page').printThis();
+    });
+});
+</script>
+
+
+</html>
