e5ae3fb3a8805f599c8d69b243fc3c831ab83c73
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-06-07 17:01:47 +0900

    Update training.php

--- training.php
+++ training.php
@@ -3,109 +3,25 @@
     include_once 'includes/dbh2.inc.php';
 
 
-    if(isset ($GET["training_id"])) {
-
-
-        $training_id = $GET["training_id"];
-        $query_contents = "SELECT * from training_form
-        where training_id = '$training_id'";
-        
-        $stmt_contents = $pdo->prepare($query_contents);
-        $stmt_contents->execute();
-       
-        $result_contents = $stmt_contents->fetchAll();
-        $usage_materials = '';
-        $training_name = '';
-    }
-
- 
 ?>
 
         <div class="training" id="training">
             <div id="table-wrapper3">
             <h4 style="text-align:center;">訓練</h4>
                 <div id="table-scroll3">
-                    <table id="trainingTable" border="1" class="table trainingT">
-                        <thead>
+                    <table id="trainingTable" class="table trainingT table-hover table-bordered rounded-3 overflow-auto">
+                        <thead class="theadstyle">
                             <tr id="firstrow">
-                                <th style="width:15%">No</th>
-                                <th style="width:30%">ファイル名</th>
-                                <th style="width:30%">Description</th>
+                                <th style="width:10%">No</th>
+                                <th style="width:25%">ファイル名</th>
+                                <th style="width:25%">Description</th>
+                                <th style="width:20%">Training Materials</th>
                                 <th style="width:25%">Completion</th>
                             </tr>
                         </thead>
+                        <tbody id="post_list">
 
-                    <?php
-                    $servername = "localhost";
-                        $username = "root";
-                        $password = "";
-                        $database = "trainingdb";
-                        
-                        //Create connection
-
-                        $connection = new mysqli($servername, $username, $password, $database);
-
-                        //Check connection
-
-                        if ($connection->connect_error) {
-                            die("Connection failed: " . $connection->connect_error);
-                        }
-                        
-                        //read all row from database table
-                        
-                        $GIDsession = $_SESSION["GID"];
-                        //$sql = "SELECT * FROM users where department = '$department';";
-                        $sql = "SELECT training_form.training_id, training_name, GID, contents, sign_progress 
-                        FROM attendance
-                        INNER JOIN training_form ON attendance.training_id = training_form.training_id
-                        WHERE attendance.GIDh  = '$GIDsession';";
-
-                        $result = $connection->query($sql);
-                        
-                        if (!$result) {
-                            die("Invalid Query: " . $connection->error);
-                        }
-
-                        //read data of each row
-                        while ($row = $result->fetch_assoc()){
-
-                        echo "<tr>
-                            <td>" . $row["training_id"] .  "</td>
-                            <td>" . $row["training_name"] .  "</td>
-                            <td>" . $row["contents"] .  "</td>
-                            
-                        ";
-                        
-                        if ($row["sign_progress"]==="1") {
-                            
-                            echo "
-                                <td>
-                                <form action = 'includes/complete.inc.php' method ='post' id='complete_training'>
-                                <input type='text' hidden name= 'training_id' value = '$row[training_id]'>
-                                <input type='text' hidden name= 'GIDfetch' value = '$GIDsession'>
-
-                               
-                                <button name='submit'>Complete</button>
-                                
-                          
-    
-                                </form>
-                                
-                                
-                                </td>";
-                            }
-                            else {
-                            echo "
-                                <td>
-                                完了
-                                </td>
-                            ";
-                            }
-                        }
-                        
-                        ?>   
-
-                    
+                        </tbody>     
                     </table>
 
                     
@@ -120,6 +36,8 @@
 
 $(document).ready(function() {
 
+contents_data();
+
 
 function contents_data() {
     //$('#post_list').html();
@@ -129,7 +47,7 @@ function contents_data() {
     var building = get_filter('building');
 
     $.ajax({
-        url: "includes/fetch_data.inc.php",
+        url: "includes/fetch_data_training.inc.php",
         method: "POST",
         data: {action:action,shift:shift,process:process,building:building},
         success:function(data){
