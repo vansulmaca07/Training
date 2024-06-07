d616b50c951fe29475d6650d57005cf5f2863ccc
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-06-07 17:06:16 +0900

    Update includes_fetch_data_progress.inc.php

--- includes_fetch_data_progress.inc.php
+++ includes_fetch_data_progress.inc.php
@@ -8,21 +8,90 @@ $department = $_SESSION["department"];
 
 if(isset($_POST["action"])) {
 
-    $query = "SELECT training_id, training_name, status_name, creator, name_, category, usage_id, date_created
+    $query = "SELECT DISTINCT training_form.training_id, training_name, status_name, creator, name_, category, usage_id, date_created, training_form.status_id
     from training_form 
-
     inner join status_ref on training_form.status_id = status_ref.status_id
     inner join users on training_form.creator = users.GID
-    where training_form.creation_department ='$department'";
+	inner join category on training_form.training_id = category.training_id
+    where condition_ ='1'";
+
+    if(isset($_POST["department_main_filter"]))
+    {   
+        $department_main_filter_trimmed = array_map('trim',$_POST["department_main_filter"]);
+        $department_main_filter = implode("','",$department_main_filter_trimmed);
+        $query .= "AND training_form.creation_department IN('".$department_main_filter."')";
+    }
+   
+    else {
+        $query .= "AND training_form.creation_department IN ('".$department."')";
+    } 
+
+    if(isset($_POST["category_main_filter"]))
+    {   
+        $category_main_filter_trimmed = array_map('trim',$_POST["category_main_filter"]);
+        $category_main_filter = implode("','",$category_main_filter_trimmed);
+        $query .= "AND category.category_id IN('".$category_main_filter."')";
+    }
+
+    if(isset($_POST["training_name_main_filter"]))
+    {
+        $training_name_main_filter = $_POST['training_name_main_filter'];
+        $query .= "AND training_name LIKE ('%$training_name_main_filter%')
+        ";
+    }
+
+    if(isset($_POST["training_id_main_filter"]))
+    {
+        $training_id_main_filter = $_POST['training_id_main_filter'];
+        $query .= "AND training_form.training_id LIKE ('%$training_id_main_filter%')
+        ";
+    }
+
+    if(isset($_POST["training_creator_main_filter"]))
+    {
+        $training_creator_main_filter = $_POST['training_creator_main_filter'];
+        $query .= "AND name_ LIKE ('%$training_creator_main_filter%')
+        ";
+    }
+ 
+    //Search Query
+
+    if(isset($_POST["training_name"]))
+    {   
+        $training_name = $_POST['training_name'];
+        $query .= "AND training_name LIKE ('%$training_name%')
+        ";
+    }
+
+    if(isset($_POST["training_creator"]))
+    {   
+        $training_creator = $_POST['training_creator'];
+        $query .= "AND name_ LIKE ('%$training_creator%')
+        ";
+    }
+
+    if(isset($_POST["training_id_search"]))
+    {   
+        $training_id_search = $_POST['training_id_search'];
+        $query .= "AND training_form.training_id LIKE ('%$training_id_search%')
+        ";
+    }
 
     if(isset($_POST["category"]))
     {   
        $category_trimmed = array_map('trim',$_POST["category"]);
        $category_filter = implode("','",$category_trimmed);
-       $query .= "AND training_form.category IN('".$category_filter."')";
+       $query .= "AND category.category_id IN('".$category_filter."')";
     }
 
-    $query .= "ORDER BY training_form.date_created ASC";
+    if(isset($_POST["status"]))
+    {   
+       $status_trimmed = array_map('trim',$_POST["status"]);
+       $status_filter = implode("','",$status_trimmed);
+       $query .= "AND training_form.status_id IN('".$status_filter."')";
+    }
+
+    $query .= "ORDER BY training_form.date_created DESC";
 
     $stmt = $pdo->prepare($query);
     $stmt->execute();
@@ -33,41 +102,32 @@ if(isset($_POST["action"])) {
     {
         foreach($result as $row)
         {
-          
-            
+                    
             $query_files =  "SELECT * FROM file_storage
             where training_id = '$row[training_id]'";
         
             $stmt2 = $pdo->prepare($query_files);
             $stmt2->execute();
             $result2 = $stmt2->fetchAll();
-
             $file_name = '';
             $file_path = '';
 
-
             $query_category = "SELECT category.category_id, category_name FROM category
-
             inner join category_ref on category_ref.category_id = category.category_id
-
             where training_id = '$row[training_id]'";
 
             $stmt3 = $pdo->prepare($query_category);
             $stmt3->execute();
             $result3 = $stmt3->fetchAll();
-
-          
-
+            
             $output .= 
                 "<tr>
                             <td style='vertical-align: middle; width: 10.1%;'><form action = 'includes/edit_form.inc.php' method ='post' id='edit_form'>
                             <input type='text' hidden name= 'training_id' value = '$row[training_id]'><button type='submit' class='btn-link'>" . $row["training_id"] .  "</button>
                             </form>
                             </td>
-
                             <td style='vertical-align: middle; font-size:20px; width:10.2%;'><a href = 'pdf_preview.php?training_id=$row[training_id]' target='_blank'><i class='bi bi-file-pdf'></i></a></button>
                             </td>
-
                             <td style='vertical-align: middle; width: 10.2%;'>" . $row["name_"] .  "</td>
                             <td style='vertical-align: middle; width: 20.3%;'>" . $row["training_name"] .  "</td>
                             <td style='vertical-align: middle; width: 10.2%;'>  
@@ -75,7 +135,7 @@ if(isset($_POST["action"])) {
 
                             foreach($result3 as $row_file) {
                                 
-                                    $output .= "$row_file[category_name],";
+                                    $output .= "$row_file[category_name]<br>";
                                     }
 
              /*   foreach($result3 as $row_file) {
@@ -85,9 +145,6 @@ if(isset($_POST["action"])) {
     
                     $output .= " <td> <a href = $file_path download>$file_name</a><br></td>";
                     } */
-    
-
-           
             
             $output .= "    </td> 
                             <td style='vertical-align: middle; width: 10.2%;'>" . $row["usage_id"] .  "</td>
@@ -110,4 +167,4 @@ if(isset($_POST["action"])) {
    
 }
 
-?>
+
