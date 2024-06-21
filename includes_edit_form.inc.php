b72c74a423f6595e0d78af64cb6fd809410c52c6
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-06-21 17:06:40 +0900

    Update includes_edit_form.inc.php

--- includes_edit_form.inc.php
+++ includes_edit_form.inc.php
@@ -2,34 +2,36 @@
 
 session_start();
 
-include("dbh.inc.php");
+//include("dbh.inc.php");
 include("dbh2.inc.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST")
+echo $_POST["training_id"];
+
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $training_id = $_POST["training_id"];
 
-    /*$sql = "SELECT * FROM training_form
-    where training_id = '$training_id';";*/
-
-    $sql = "SELECT training_id, training_name, process_prefix, process_suffix,
+    $query_main = "SELECT training_id, training_name, process_prefix, process_suffix,
     start_time_regular, end_time_regular, location_regular, instructor_regular, count_, date_created,
     category_quality, category_environment, category_safety_and_hygiene, category_others, category_others_manual, purpose, contents, usage_id, audience, area, confirmation_by, confirmation_date, department_name, name_, start_time_regular,
-    checker_comment_regular, checker_people_regular, checker_date_regular, modified_date, status_id, creation_department
+    checker_comment_regular, checker_people_regular, checker_date_regular, modified_date, status_id, creation_department, start_time_a, end_time_a, location_a
     FROM training_form
 
     INNER JOIN department on training_form.creation_department = department.department_id
     INNER JOIN users on training_form.creator = users.GID
-    where training_id = '$training_id';";
-                            
-    $result = $conn->query($sql);
-                            
-    if (!$result) {
-    die("Invalid Query: " . $connection->error);
-    }
+    where training_id = :training_id";
 
-    while($row = $result->fetch_assoc()) {
 
+    $stmt_main = $pdo->prepare($query_main);
+
+    $stmt_main->bindParam(":training_id", $training_id);
+    $stmt_main->execute();
+                            
+    $result = $stmt_main->fetchAll();
+                           
+    foreach($result as $row) {
+    
     $_SESSION["creation_department"] = $row["creation_department"];
     $_SESSION["training_id"] = $row["training_id"];
     $_SESSION["training_name"] = $row["training_name"];
@@ -39,6 +41,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $_SESSION["end_time_regular"] = $row["end_time_regular"];
     $_SESSION["location_regular"] = $row["location_regular"];
     $_SESSION["instructor_regular"] = $row["instructor_regular"];
+
+    $_SESSION["start_time_a"] = $row["start_time_a"];
+    $_SESSION["end_time_a"] = $row["end_time_a"];
+    $_SESSION["location_a"] = $row["location_a"];
+    $_SESSION["instructor_a"] = $row["instructor_a"];
+
     $_SESSION["category_quality"] = $row["category_quality"];
     $_SESSION["category_environment"] = $row["category_environment"];
     $_SESSION["category_safety_and_hygiene"] = $row["category_safety_and_hygiene"];
@@ -59,8 +67,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $_SESSION["checker_comment_regular"] = $row["checker_comment_regular"];
     $_SESSION["checker_date_regular"] = $row["checker_date_regular"];
     $_SESSION["modified_date"] = $row["modified"];
-    $_SESSION["status_id"] = $row["status_id"];
+    $_SESSION["status_id"] = $row["status_id"]; 
 
+    }
+
+    $stmt_main->closeCursor(); 
 
     $query = "SELECT category.category_id, category_name FROM category
     
@@ -83,7 +94,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     
     $_SESSION["category"] = $category;
 
+    
+
     header("location: ../editform.php");
 
+    
+
+    echo $training_id;
     exit();
-    }
+
+    } 
