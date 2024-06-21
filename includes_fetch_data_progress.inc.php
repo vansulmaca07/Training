6e8ef3d4e5ba19f54a3e9afe9ef615b0f950ac81
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-06-21 17:07:37 +0900

    Update includes_fetch_data_progress.inc.php

--- includes_fetch_data_progress.inc.php
+++ includes_fetch_data_progress.inc.php
@@ -17,9 +17,13 @@ if(isset($_POST["action"])) {
 
     if(isset($_POST["department_main_filter"]))
     {   
+        
         $department_main_filter_trimmed = array_map('trim',$_POST["department_main_filter"]);
         $department_main_filter = implode("','",$department_main_filter_trimmed);
         $query .= "AND training_form.creation_department IN('".$department_main_filter."')";
+
+        $_SESSION["department_main_filter"] =   $department_main_filter_trimmed; //set session filter for main department
+
     }
    
     else {
@@ -31,13 +35,16 @@ if(isset($_POST["action"])) {
         $category_main_filter_trimmed = array_map('trim',$_POST["category_main_filter"]);
         $category_main_filter = implode("','",$category_main_filter_trimmed);
         $query .= "AND category.category_id IN('".$category_main_filter."')";
+
+        $_SESSION["category_main_filter"] = $category_main_filter_trimmed; //set session filter for main department
     }
 
     if(isset($_POST["training_name_main_filter"]))
     {
         $training_name_main_filter = $_POST['training_name_main_filter'];
-        $query .= "AND training_name LIKE ('%$training_name_main_filter%')
-        ";
+        $query .= "AND training_name LIKE ('%$training_name_main_filter%')";
+
+        $_SESSION["training_name_main_filter"] = $training_name_main_filter;
     }
 
     if(isset($_POST["training_id_main_filter"]))
@@ -45,6 +52,8 @@ if(isset($_POST["action"])) {
         $training_id_main_filter = $_POST['training_id_main_filter'];
         $query .= "AND training_form.training_id LIKE ('%$training_id_main_filter%')
         ";
+
+        $_SESSION["training_id_main_filter"] = $training_id_main_filter;
     }
 
     if(isset($_POST["training_creator_main_filter"]))
@@ -52,6 +61,8 @@ if(isset($_POST["action"])) {
         $training_creator_main_filter = $_POST['training_creator_main_filter'];
         $query .= "AND name_ LIKE ('%$training_creator_main_filter%')
         ";
+
+        $_SESSION["training_creator_main_filter"] = $training_creator_main_filter;
     }
  
     //Search Query
@@ -61,6 +72,8 @@ if(isset($_POST["action"])) {
         $training_name = $_POST['training_name'];
         $query .= "AND training_name LIKE ('%$training_name%')
         ";
+
+        $_SESSION["training_name_filter"] = $training_name;
     }
 
     if(isset($_POST["training_creator"]))
@@ -68,6 +81,10 @@ if(isset($_POST["action"])) {
         $training_creator = $_POST['training_creator'];
         $query .= "AND name_ LIKE ('%$training_creator%')
         ";
+
+        $_SESSION["training_creator_filter"] = $training_creator;
+
+      
     }
 
     if(isset($_POST["training_id_search"]))
@@ -75,6 +92,8 @@ if(isset($_POST["action"])) {
         $training_id_search = $_POST['training_id_search'];
         $query .= "AND training_form.training_id LIKE ('%$training_id_search%')
         ";
+
+        $_SESSION["training_id_filter"] = $training_id_search;
     }
 
     if(isset($_POST["category"]))
@@ -82,6 +101,8 @@ if(isset($_POST["action"])) {
        $category_trimmed = array_map('trim',$_POST["category"]);
        $category_filter = implode("','",$category_trimmed);
        $query .= "AND category.category_id IN('".$category_filter."')";
+
+       $_SESSION["category_filter"] = $category_trimmed;
     }
 
     if(isset($_POST["status"]))
@@ -89,6 +110,8 @@ if(isset($_POST["action"])) {
        $status_trimmed = array_map('trim',$_POST["status"]);
        $status_filter = implode("','",$status_trimmed);
        $query .= "AND training_form.status_id IN('".$status_filter."')";
+
+       $_SESSION["status_filter"] = $status_trimmed;
     }
 
     $query .= "ORDER BY training_form.date_created DESC";
@@ -121,17 +144,19 @@ if(isset($_POST["action"])) {
             $result3 = $stmt3->fetchAll();
             
             $output .= 
-                "<tr>
-                            <td style='vertical-align: middle; width: 10.1%;'><form action = 'includes/edit_form.inc.php' method ='post' id='edit_form'>
+                "<tr style='max-height: 70px; overflow: auto;' >
+                            <td style='vertical-align: middle; width: 10.1%;'><form action = 'includes/edit_form.inc.php' method = 'POST' id='edit_form'>
                             <input type='text' hidden name= 'training_id' value = '$row[training_id]'><button type='submit' class='btn-link'>" . $row["training_id"] .  "</button>
                             </form>
                             </td>
                             <td style='vertical-align: middle; font-size:20px; width:10.2%;'><a href = 'pdf_preview.php?training_id=$row[training_id]' target='_blank'><i class='bi bi-file-pdf'></i></a></button>
                             </td>
                             <td style='vertical-align: middle; width: 10.2%;'>" . $row["name_"] .  "</td>
-                            <td style='vertical-align: middle; width: 20.3%;'>" . $row["training_name"] .  "</td>
-                            <td style='vertical-align: middle; width: 10.2%;'>  
-                           ";
+                            <td style='vertical-align: middle; width: 20.3%; word-break: break-all'>" . $row["training_name"] .  "</td>
+                            <td style='vertical-align: middle; width: 10.2%; word-break: break-all'>  
+                           "; 
+
+                         
 
                             foreach($result3 as $row_file) {
                                 
@@ -149,10 +174,10 @@ if(isset($_POST["action"])) {
             $output .= "    </td> 
                             <td style='vertical-align: middle; width: 10.2%;'>" . $row["usage_id"] .  "</td>
                             <td style='vertical-align: middle; width:15.1% ;'>" . $row["status_name"] .  "</td>
-                            <td style='vertical-align: middle; width: ;'>
+                            <td style='vertical-align: middle; width:13.7% ;'>
                             <form action='includes/documentNo.inc.php' method ='POST'>
                             <input type='text' hidden value='$row[training_id]' name='documentNo'>             
-                            <button type='submit' class='btn-attendance'><span>サイン</span></button>
+                            <button type='submit' class='btn btn-primary'><span>サイン</span></button>
                             </form>
                             </td>
                 </tr>";
